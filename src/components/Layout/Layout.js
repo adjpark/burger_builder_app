aeaa6fefa9fc205c5c0347f9582dc57473f877a1
Andrew Park <andrew.dj.park@gmail.com>
2018-03-14 16:46:51 -0700

    finish Burger and BurgerBuilder components

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import Aux from '../../hoc/Aux';
+import styles from './Layout.css';
 
 const Layout =  ( props ) => (
     <Aux>
         <div>Toolbar SideDrawer Backrop</div>
-        <main>
+        <main className={styles.Content}>
             {props.children}
         </main>
     </Aux>
